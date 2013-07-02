5bbdc52342f89d9fe3d732724c14de06b9d3e013
foo <foo@foo.com>
2013-07-02 18:33:17 +0900

    init

---
+++ getfeeds.php
@@ -0,0 +1,42 @@
+<?php
+//昨日のはてブのフィードを取得してハンチングブログにEntryする
+mb_internal_encoding("utf-8");
+$ini = parse_ini_file("config.ini");
+$date = (!empty($_GET['date']))?$_GET['date']:date('Ymd');
+$atom = file_get_contents("http://b.hatena.ne.jp/".$ini['hatenaid']."/atomfeed?date=".$date);
+$atomobj = simplexml_load_string($atom);
+$title = "";
+$content = "";
+$dateCreated = "";
+foreach($atomobj->entry as $entrydata) {
+  $content .= $entrydata->content;
+  $dateCreated = $entrydata->issued;
+}
+if($content=="") {
+  print 'NO BOOKMARK DATE='.$date;
+  file_put_contents("log.txt", date("Y-m-d H:i:s")." NO BOOKMARK\n", FILE_APPEND | LOCK_EX);
+  return;
+} else {
+  //trimming
+  $content = str_replace('<a ', '<a target="_blank" ', $content);
+  $title = "ブックマーク ".date('Y年n月j日', strtotime($dateCreated));
+}
+$data = array(
+  'blogid' => $ini['blogid'],
+  'authorid' => $ini['authorid'],
+  'title' => $title,
+  'text' => $content,
+  'status' => ($ini['status'])?$ini['status']:'release',
+);
+// POST
+$url = $ini['postcgi'];
+$options = array('http' => array(
+  'method' => 'POST',
+  'header' => 'User-Agent: Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36',
+  'content' => http_build_query($data),
+));
+$contents = file_get_contents($url, false, stream_context_create($options));
+$logbody = ereg_replace("\r|\n"," ",$body);
+file_put_contents("log.txt", date("Y-m-d H:i:s")." ".$subject." ".$logbody."\n", FILE_APPEND | LOCK_EX);
+print $contents;
+?>
